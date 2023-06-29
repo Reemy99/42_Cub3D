a15be7c26ace8ca9a0016ac21aadf4fdd3f9c435
Reem Aldahhan <reemaldahhan@Reems-MacBook-Pro.local>
2023-06-29 18:25:23 +0400

    math

--- raycasting/math.c
+++ raycasting/math.c
@@ -1,34 +1,44 @@
-
-
 #include "../include/include.h"
 
 int	is_up(double ang)
 {
-	if (ang >= M_PI && ang <= (2 * M_PI))
+	if (ang >= M_PI && ang <= (2 * M_PI)) // 180 and 360 degrees.
 		return (1);
 	return (0);
 }
-
+//__________________________________________
 int	is_left(double ang)
 {
-	if (ang >= (M_PI / 2) && ang <= ((3 * M_PI) / 2))
+	if (ang >= (M_PI / 2) && ang <= ((3 * M_PI) / 2)) //between 90 degrees and 270 
 		return (1);
 	return (0);
 }
-
-unsigned int	get_floor_c(t_all *data)
+//__________________________________________
+unsigned int	floor_color(t_all *data)
 {
-	return (data->valid.floor[0] * 65536 + \
-	data->valid.floor[1] * 256 + data->valid.floor[2]);
+	unsigned int red = data->valid.floor[0];
+	unsigned int green = data->valid.floor[1];
+	unsigned int blue = data->valid.floor[2];
+	
+	unsigned int colorValue = (red << 16) + (green << 8) + blue;
+	
+	return colorValue;
 }
-
+//__________________________________________
+// Pythagorean theorem "fethagorse"
 double	calculate_distance(t_all *data, double y, double x)
 {
 	return (sqrt(pow(x - data->x_player, 2) + pow(y - data->y_player, 2)));
 }
-
-unsigned int	get_ceiling_c(t_all *data)
+//__________________________________________
+unsigned int	ceiling_color(t_all *data)
 {
-	return (data->valid.ceiling[0] * 65536 + \
-	data->valid.ceiling[1] * 256 + data->valid.ceiling[2]);
+	unsigned int red = data->valid.ceiling[0];
+	unsigned int green = data->valid.ceiling[1];
+	unsigned int blue = data->valid.ceiling[2];
+	
+	unsigned int colorValue = (red << 16) + (green << 8) + blue;
+	
+	return colorValue;
 }
+
